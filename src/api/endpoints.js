6c2fbe52de2ef555b801a9d9f34397b289801284
Phung <phungtr59@gmail.com>
2025-10-17 00:12:51 +0700

    Update endpoints.js

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -11,6 +11,7 @@ const endpoints = {
     dashboard: "/v1/Admin/dashboard",
     users: "/v1/UserProfile",
     dealers: "/v1/Dealers",
+    vehicles: "/v1/VehiclesModels",
   },
   dealer: {
     vehicles: "/v1/Dealer/vehicles",
