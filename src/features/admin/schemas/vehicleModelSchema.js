efdd7af52da7f88792095fb5516657fb88041397
Phung <phungtr59@gmail.com>
2025-10-16 21:25:05 +0700

    Create vehicleModelSchema.js

---
+++ src/features/admin/schemas/vehicleModelSchema.js
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const vehicleModelSchema = z.object({
+  code: z.string().min(1, "Code is required"),
+  name: z.string().min(1, "Name is required"),
+  launchDate: z.any().optional(),
+  description: z.string().optional(),
+  status: z.boolean().default(true),
+  ranking: z.enum(["Standard", "Premium", "Luxury"]).default("Standard"),
+});
+
+export default vehicleModelSchema;
+
+
