15b68962c61891b927468ad1e321c409f04c488a
Phung <phungtr59@gmail.com>
2025-10-16 14:02:49 +0700

    Create ChangePasswordPage.jsx

---
+++ src/features/auth/pages/ChangePasswordPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChangePasswordForm from "../components/ChangePasswordForm";
+
+const ChangePasswordPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4 bg-gray-50">
+      <ChangePasswordForm />
+    </div>
+  );
+};
+
+export default ChangePasswordPage;
