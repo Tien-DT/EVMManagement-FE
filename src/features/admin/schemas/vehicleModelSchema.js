50aff712b0a5214fe4204e1b2cc7a37dfe5f4df4
Phung <phungtr59@gmail.com>
2025-10-16 21:34:44 +0700

    Update vehicleModelSchema.js

--- src/features/admin/schemas/vehicleModelSchema.js
+++ src/features/admin/schemas/vehicleModelSchema.js
@@ -6,7 +6,7 @@ export const vehicleModelSchema = z.object({
   launchDate: z.any().optional(),
   description: z.string().optional(),
   status: z.boolean().default(true),
-  ranking: z.enum(["Standard", "Premium", "Luxury"]).default("Standard"),
+  ranking: z.number().int().min(0).max(2).default(0),
 });
 
 export default vehicleModelSchema;
