ba1028844c23808b905cd2b3d3a4fe566b8973bc
Phung <phungtr59@gmail.com>
2025-10-17 14:43:41 +0700

    Update endpoints.js

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -5,7 +5,11 @@ const endpoints = {
     login: "/v1/Auth/login",
     refresh: "/v1/Auth/refresh",
     forgotPassword: "/v1/Auth/forgot-password",
+    verifyOtp: "/v1/Auth/verify-otp",
     resetPassword: "/v1/Auth/reset-password",
+  },
+  userProfile: {
+    byAccount: (accountId) => `/v1/UserProfile/by-account/${accountId}`,
   },
   // thêm các nhóm endpoint khác ở đây
   admin: {
