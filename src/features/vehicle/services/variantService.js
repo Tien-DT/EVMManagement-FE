edb3d528aab8f1ac7ff9afa52e86a14b9755abd3
Phung <phungtr59@gmail.com>
2025-10-20 23:01:28 +0700

    Create variantService.js

---
+++ src/features/vehicle/services/variantService.js
@@ -0,0 +1,50 @@
+import axiosInstance from "../../../api/axiosInstance";
+import endpoints from "../../../api/endpoints";
+
+const basePath = endpoints.admin.vehicleVariants;
+
+export const variantService = {
+  list: async (params = {}) => {
+    try {
+      return await axiosInstance.get(basePath, { params });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  getById: async (id) => {
+    try {
+      return await axiosInstance.get(`${basePath}/${id}`);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  create: async (payload) => {
+    try {
+      return await axiosInstance.post(basePath, payload);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  update: async (id, payload) => {
+    try {
+      return await axiosInstance.put(`${basePath}/${id}`, payload);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  remove: async (id) => {
+    try {
+      return await axiosInstance.delete(`${basePath}/${id}`);
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default variantService;
+
+
